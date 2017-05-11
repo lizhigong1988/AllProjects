d5933c1fa8b9d21f41664f19b0c7e666390631c1
lizhigong1988 <306128847@qq.com>
2017-05-11 09:08:47 +0800

    修改代码

--- 20170509_emoji解忧杂货铺/emoji解忧杂货铺/emoji解忧杂货铺/SecondFloor.xaml.cs
+++ 20170509_emoji解忧杂货铺/emoji解忧杂货铺/emoji解忧杂货铺/SecondFloor.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Drawing.Printing;
+using System.Drawing;
+using System.IO;
 
 namespace emoji解忧杂货铺
 {
@@ -31,7 +34,7 @@ namespace emoji解忧杂货铺
 
         private void bd1_1_MouseEnter(object sender, MouseEventArgs e)
         {
-            bd1_1.Background = Brushes.Transparent;
+            bd1_1.Background = System.Windows.Media.Brushes.Transparent;
             bdShadow1_1.Background = new ImageBrush(new BitmapImage(new Uri("Images/SecondFloorButtonShadowCorver.png", UriKind.Relative)));
         }
 
@@ -43,7 +46,7 @@ namespace emoji解忧杂货铺
 
         private void bd1_2_MouseEnter(object sender, MouseEventArgs e)
         {
-            bd1_2.Background = Brushes.Transparent;
+            bd1_2.Background = System.Windows.Media.Brushes.Transparent;
             bdShadow1_2.Background = new ImageBrush(new BitmapImage(new Uri("Images/SecondFloorButtonShadowCorver.png", UriKind.Relative)));
         }
 
@@ -55,7 +58,7 @@ namespace emoji解忧杂货铺
 
         private void bd1_3_MouseEnter(object sender, MouseEventArgs e)
         {
-            bd1_3.Background = Brushes.Transparent;
+            bd1_3.Background = System.Windows.Media.Brushes.Transparent;
             bdShadow1_3.Background = new ImageBrush(new BitmapImage(new Uri("Images/SecondFloorButtonShadowCorver.png", UriKind.Relative)));
         }
 
@@ -68,7 +71,7 @@ namespace emoji解忧杂货铺
 
         private void bd2_1_MouseEnter(object sender, MouseEventArgs e)
         {
-            bd2_1.Background = Brushes.Transparent;
+            bd2_1.Background = System.Windows.Media.Brushes.Transparent;
             bdShadow2_1.Background = new ImageBrush(new BitmapImage(new Uri("Images/SecondFloorButtonShadowCorver.png", UriKind.Relative)));
         }
 
@@ -80,7 +83,7 @@ namespace emoji解忧杂货铺
 
         private void bd2_2_MouseEnter(object sender, MouseEventArgs e)
         {
-            bd2_2.Background = Brushes.Transparent;
+            bd2_2.Background = System.Windows.Media.Brushes.Transparent;
             bdShadow2_2.Background = new ImageBrush(new BitmapImage(new Uri("Images/SecondFloorButtonShadowCorver.png", UriKind.Relative)));
         }
 
@@ -92,7 +95,7 @@ namespace emoji解忧杂货铺
 
         private void bd2_3_MouseEnter(object sender, MouseEventArgs e)
         {
-            bd2_3.Background = Brushes.Transparent;
+            bd2_3.Background = System.Windows.Media.Brushes.Transparent;
             bdShadow2_3.Background = new ImageBrush(new BitmapImage(new Uri("Images/SecondFloorButtonShadowCorver.png", UriKind.Relative)));
         }
 
@@ -102,10 +105,53 @@ namespace emoji解忧杂货铺
             bdShadow2_3.Background = new ImageBrush(new BitmapImage(new Uri("Images/SecondFloorButtonShadow.png", UriKind.Relative)));
         }
 
+        private enum DrawKinds
+        {
+            DRAW_KINDS_STRING,
+            DRAW_KINDS_LINE,
+        }
+        private class DrawElement
+        {
+            public string drawText { get; set; }
+            public DrawKinds drawKind { get; set; }
+            public Font font { get; set; }
+            public RectangleF rect { get; set; }
+            public StringFormat sf { get; set; }
+        };
+        List<List<DrawElement>> drawPageList = new List<List<DrawElement>>();
 
+        float _width = 5.8F * 39.36F;
+        float _height = 9F * 39.36F;
         private void bd_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            PrintDocument printDoc = new PrintDocument();
+            //设置打印用的纸张 当设置为Custom的时候，可以自定义纸张的大小，还可以选择A4,A5等常用纸型
+            //printDoc.DefaultPageSettings.PaperSize = new PaperSize("Custum", 500, 300);
+            PaperSize paperSize = new PaperSize("Custum", (int)_width, (int)_height);
+            printDoc.DefaultPageSettings.PaperSize = paperSize;
+            printDoc.DocumentName = "打印报表";
+            drawPageList.Clear();
+            bool isHLandscape = false;
+            printDoc.DefaultPageSettings.Landscape = isHLandscape;//true横向 false纵向
+            printDoc.PrintPage += new PrintPageEventHandler(PrintDocument_PrintPage);
+            //printDoc.Print();
 
+            System.Windows.Forms.PrintPreviewDialog printPreDlg = new System.Windows.Forms.PrintPreviewDialog();
+            //将写好的格式给打印预览控件以便预览
+            printPreDlg.Document = printDoc;
+            (printPreDlg as System.Windows.Forms.Form).WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            printPreDlg.PrintPreviewControl.Zoom = 1.0;
+            //显示打印预览
+            printPreDlg.ShowDialog();
+        }
+
+        private void PrintDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            string[] files = Directory.GetFiles(System.Environment.CurrentDirectory + "\\PrintLibs");
+            int index = new Random().Next(0, files.Length);
+            System.Drawing.Image printImage = System.Drawing.Image.FromFile(files[index]);
+            e.Graphics.DrawImage(printImage, 20, 20, _width - 40, _height - 40);
+            e.HasMorePages = false;
         }
     }
 }
